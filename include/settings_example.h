e5f52e9a1b3bbc74f419b80d996a60ef70b5ca42
KaiFassnacht <fkai@gmx.de>
2025-12-30 13:09:07 +0100

    Neue Änderungen an der Datei include/settings.h:

--- include/settings_example.h
+++ include/settings_example.h
@@ -3,46 +3,83 @@
 
 #include <Arduino.h>
 
-// --- Hardware Pins (Definition der verfügbaren GPIOs) ---
-#define PIN_A  2
-#define PIN_B  4
-#define PIN_C  12
-#define PIN_D  14
-#define PIN_E  15
-#define PIN_F  32
-#define PIN_G  33
-#define UNUSED -1
+// --- Hardware Pins ---
+// constexpr ist typsicher und wird zur Kompilierzeit aufgelöst
+namespace Pins {
+    constexpr int8_t A = 2;
+    constexpr int8_t B = 4;
+    constexpr int8_t C = 12;
+    constexpr int8_t D = 14;
+    constexpr int8_t E = 15;
+    constexpr int8_t F = 32;
+    constexpr int8_t G = 33;
+    constexpr int8_t UNUSED = -1;
+}
 
-// --- Tasten-Zuweisung (Mapping) ---
-// Index 0 ist Taste '0', Index 1 ist Taste '1', usw.
-// Hier kannst du nun beliebig schieben:
-inline const int TASTEN_MAP[10] = {
-    PIN_A,  // Taste '0' -> GPIO 2
-    PIN_B,  // Taste '1' -> GPIO 4
-    PIN_C,  // Taste '2' -> GPIO 12
-    PIN_D,  // Taste '3' -> GPIO 14
-    PIN_E,  // Taste '4' -> GPIO 15
-    PIN_F,  // Taste '5' -> GPIO 32
-    PIN_G,  // Taste '6' -> GPIO 33
-    UNUSED, // Taste '7'
-    UNUSED, // Taste '8'
-    UNUSED  // Taste '9'
+// --- Tasten-Zuweisung ---
+// Mit constexpr direkt im Header definierbar
+constexpr int TASTEN_MAP[10] = {
+    Pins::A, Pins::B, Pins::C, Pins::D, Pins::E, 
+    Pins::F, Pins::G, Pins::UNUSED, Pins::UNUSED, Pins::UNUSED
 };
 
 // --- Zeitsteuerung ---
-#define RELAIS_DAUER 3000   // Zeit in ms
-#define INAKTIVITAETS_TIMEOUT 5000 // 10 Sekunden in ms
-
+namespace Timing {
+    constexpr uint32_t RELAIS_DAUER = 3000;
+    constexpr uint32_t INAKTIVITAETS_TIMEOUT = 5000;
+}
 
 // --- SIP Einstellungen ---
-inline const char* sip_user = "<sip user>>";
-inline const char* sip_password = "<passwort>";
-inline const char* fritzbox_ip = "<Fritzbox IP>";
-inline const int sip_port = 5060;
-inline const int rtp_port = 10002;
+namespace SipConfig {
+    constexpr const char* USER = "<SIPuser>";
+    constexpr const char* PASSWORD = "<SIPpasswort>";
+    constexpr const char* SERVER_IP = "<FritzboxIP>";
+    constexpr uint16_t PORT = 5060;
+    constexpr uint16_t RTP_PORT = 10002;
+}
 
 
+// --- Audio Signale Konfiguration ---
+struct ToneStep {
+    uint16_t freq;
+    uint16_t duration;
+    uint16_t pause; // Pause NACH dem Ton
+};
 
+namespace AudioSequences {
+    // OK-Ton: 2 ansteigende Töne
+    const ToneStep OK[] = {
+        {660, 150, 20},
+        {880, 150, 0}
+    };
+
+    // Error-Ton: 2 abfallende Töne
+    const ToneStep ERR[] = {
+        {300, 200, 20},
+        {200, 200, 0}
+    };
+
+    // Alarm: 3 abfallende Töne (Sirene)
+    const ToneStep ALARM[] = {
+        {1000, 80, 0},
+        {800, 80, 0},
+        {600, 80, 0}
+    };
+
+    // Start-Ton: Einzelner Piep
+    const ToneStep START[] = {
+        {440, 100, 0}
+    };
+
+    // Warnung vor dem Auflegen: 2 Töne mit deutlicher Pause
+    const ToneStep TIMEOUT[] = {
+        {150, 60, 100}, // Erster Schlag (tief), 100ms Pause
+        {150, 60, 0},    // Zweiter Schlag    };
+        {150, 60, 100}, // Erster Schlag (tief), 100ms Pause
+        {150, 60, 0}    // Zweiter Schlag    };
+    };
+
+}
 
 
 #endif
